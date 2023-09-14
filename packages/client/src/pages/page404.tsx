a20d05c444d84f3c9d48743027aa94107cf1f801
Eduard Ispolatov <u281094@gmail.com>
2023-09-15 00:54:58 +0300

    #MF3-21 add MUI for Page404

--- packages/client/src/pages/page404.tsx
+++ packages/client/src/pages/page404.tsx
@@ -1,17 +1,29 @@
 import { FC } from 'react'
+import { Box, Button, Typography } from '@mui/material'
+import { green } from '@mui/material/colors'
+
+const primary = green[500]
 
 type Page404Props = {
   description: string
 }
 
-export const Page404: FC<Page404Props> = ({ description }) => {
-  const descriptionContent = description ? <div>{description}</div> : null
-
-  return (
-    <div>
-      <div>404</div>
-      <div>Страница не найдена</div>
-      {descriptionContent}
-    </div>
-  )
-}
+export const Page404: FC<Page404Props> = ({ description }) => (
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      minHeight: '100vh',
+      backgroundColor: primary,
+    }}>
+    <Typography variant="h1" style={{ color: 'white' }}>
+      404
+    </Typography>
+    <Typography variant="h6" style={{ color: 'white' }}>
+      {description || 'Страница, которую вы ищете, не существует.'}
+    </Typography>
+    <Button variant="contained">Вернуться назад</Button>
+  </Box>
+)
