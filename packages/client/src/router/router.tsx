abce93a99127f96de451c178406e9a77e060135a
sigarachi <lalalabtv@gmail.com~>
2023-09-23 16:40:51 +0300

    MF3-39 Прогнал файлы через format

--- packages/client/src/router/router.tsx
+++ packages/client/src/router/router.tsx
@@ -4,6 +4,7 @@ import { AboutGame } from '../pages/aboutGame/AboutGame'
 import { IRouter } from './interfaces'
 import { SingUp } from '../pages/singUp/SingUp'
 import { SingIn } from '../pages/singIn/SingIn'
+import { LeaderBoard } from '../pages'
 
 export const Router = ({ isAuthorized }: IRouter) => {
   return (
@@ -13,14 +14,18 @@ export const Router = ({ isAuthorized }: IRouter) => {
           <Route path={'/profile'} element={<>Страница профиля</>} />
           <Route path={'/'} element={<>Страница игры</>} />
           <Route path={'/about'} element={<AboutGame />} />
-          <Route path={'/leaderboard'} element={<>Страница лидерборда</>} />
+          <Route path={'/leaderboard'} element={<LeaderBoard />} />
           <Route path={'/forum'} element={<>Страница форума</>}>
             <Route path={':topicId'} element={<>Страница топика</>} />
           </Route>
         </>
       )}
-      <Route path={'/sign-in'} element={<SingIn />} />
-      <Route path={'/sign-up'} element={<SingUp />} />
+      {!isAuthorized && (
+        <>
+          <Route path={'/sign-in'} element={<SingIn />} />
+          <Route path={'/sign-up'} element={<SingUp />} />
+        </>
+      )}
       <Route
         path="*"
         element={
