c97739737a895ad858f4962dc514f6715c2df981
gudtae <90135960+gudtae@users.noreply.github.com>
2023-09-24 20:56:53 +0300

    mf3-32 validation add:добавлена схема валидации (#13)

--- packages/client/src/constants/validationSchema.ts
+++ packages/client/src/constants/validationSchema.ts
@@ -1,19 +1,43 @@
 import * as Yup from 'yup'
 
 export const singUpvalidationSchema = Yup.object({
-  first_name: Yup.string().required('Обязательное поле'),
-  second_name: Yup.string().required('Обязательное поле'),
-  login: Yup.string().required('Обязательное поле'),
+  first_name: Yup.string()
+    .matches(
+      /^[A-ZА-Я][a-zA-Zа-яА-Я-]*$/,
+      'Первая буква должна быть заглавной, без цифр, пробелов, разрешен дефис'
+    )
+    .required('Обязательное поле'),
+  second_name: Yup.string()
+    .matches(
+      /^[A-ZА-Я][a-zA-Zа-яА-Я-]*$/,
+      'Первая буква должна быть заглавной, без цифр, пробелов, разрешен дефис'
+    )
+    .required('Обязательное поле'),
+  login: Yup.string()
+    .matches(
+      /^(?!_)(?!.*?_$)(?!^[0-9]*$)[a-zA-Z0-9_]{3,20}$/,
+      'Латиница, разрешен дефис и нижнее подчеркивание'
+    )
+    .required('Обязательное поле'),
   email: Yup.string()
-    .email('Неверный формат email')
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+      'Неправильный формат email'
+    )
     .required('Обязательное поле'),
   password: Yup.string()
-    .min(6, 'Пароль должен содержать минимум 6 символов')
+    .min(8, 'Пароль должен содержать минимум 8 символов')
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,40}$/,
+      'Пароль должен содержать хотя бы одну заглавную букву и цифру'
+    )
     .required('Обязательное поле'),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password')], 'Пароли должны совпадать')
     .required('Обязательное поле'),
-  phone: Yup.string().required('Обязательное поле'),
+  phone: Yup.string()
+    .matches(/^(\+)?\d{10,15}$/, 'Неправильный формат номера телефона')
+    .required('Обязательное поле'),
 })
 
 export const newTopicValidationSchema = Yup.object({
@@ -32,5 +56,11 @@ export const singInValidationSchema = Yup.object({
 
 export const changePasswordValidationSchema = Yup.object({
   oldPassword: Yup.string().required('Обязательное поле'),
-  newPassword: Yup.string().required('Обязательное поле'),
+  newPassword: Yup.string()
+    .min(8, 'Пароль должен содержать минимум 8 символов')
+    .matches(
+      /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,40}$/,
+      'Пароль должен содержать хотя бы одну заглавную букву и цифру'
+    )
+    .required('Обязательное поле'),
 })
