921c1006b6b84f8345f4e9104941b7475f063d47
Eduard Ispolatov <u281094@gmail.com>
2023-10-10 01:16:12 +0300

    #MF3-57 fix

--- packages/client/src/pages/GameMenu/index.tsx
+++ packages/client/src/pages/GameMenu/index.tsx
@@ -5,21 +5,16 @@ import { Container, Button } from '@mui/material'
 import { Game } from '../Game'
 import { useNavigate } from 'react-router-dom'
 import { RouterName } from '../../router/types'
+import { useFullScreen } from '../../hooks/useFullScreen'
 
 const boardId = 'boardId'
-
-enum FullScreenText {
-  full = 'на полный экран',
-  default = 'уменьшить экран',
-}
 
 export function GameMenu() {
   const [showCountdown, setShowCountdown] = useState(true)
   const [isGameOver, setIsGameOver] = useState(false)
   const [score, setScore] = useState<number>(0)
   const navigate = useNavigate()
-  const [textFullScreenButton, setTextFullScreenButton] =
-    useState<FullScreenText>(FullScreenText.full)
+  const [changeFullScreen, textContent] = useFullScreen()
 
   const handleEndCountdown = () => {
     setShowCountdown(false)
@@ -43,19 +38,9 @@ export function GameMenu() {
     return <Countdown onEnd={handleEndCountdown} />
   }
 
-  const handleClickFullScreen = () => {
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen()
-      setTextFullScreenButton(FullScreenText.default)
-    } else if (document.exitFullscreen) {
-      document.exitFullscreen()
-      setTextFullScreenButton(FullScreenText.full)
-    }
-  }
-
   return (
     <Container style={{ display: 'flex', justifyContent: 'center' }}>
-      <Button onClick={handleClickFullScreen}>{textFullScreenButton}</Button>
+      <Button onClick={changeFullScreen}>{textContent}</Button>
       <Game boardId={boardId} callbackEndGame={handleEndGame} />
       {isGameOver ? (
         <GameEnd
