723cc1c6924173bb02eface2e993209c624cb8d2
Taisia <alleanoraanges@gmail.com>
2023-12-03 23:17:53 +0300

    change global

--- packages/client/src/globals.ts
+++ packages/client/src/globals.ts
@@ -1,6 +1,8 @@
 export const BASE_URL =
   import.meta.env.VITE_API_URL || 'https://ya-praktikum.tech/api/v2'
 
-export const BASE_API_URL = 'http://localhost:3001/api/v1'
+export const BASE_API_URL = import.meta.env.DEV
+  ? 'http://localhost:3001/api/v1'
+  : 'http://edem-v-magadan-30.ya-praktikum.tech:3001/api/v1'
 
 export const HOST_URL = 'http://localhost:3000'
